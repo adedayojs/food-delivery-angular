89b7d10a400f12d417c56656a9f50e07e8e20547
adedayojs <samfeolu@gmail.com>
2019-09-30 04:09:06 +0100

    Addition: Created component to display recommened section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { RecommendedComponent } from './recommended/recommended.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    RecommendedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recommended/recommended.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended',
+  templateUrl: './recommended.component.html',
+  styleUrls: ['./recommended.component.css']
+})
+export class RecommendedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

0a09bc62e7c3655094547f7b071a0cd9921b99b7
adedayojs <samfeolu@gmail.com>
2019-09-30 03:28:59 +0100

    Fix:Edited and Implemented the view for jumbotron in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,

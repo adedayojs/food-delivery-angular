24b20e8a55839381afc8c0b8ab2d1491edad4e88
adedayojs <samfeolu@gmail.com>
2019-09-30 03:30:09 +0100

    Fix: Added Stale data to component

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jumbotron.component.css']
 })
 export class JumbotronComponent implements OnInit {
+  cities = [
+    { city: 'Lagos', area: ['Ikorodu', 'Badagry', 'Lekki', 'Victoria Island', 'Ajah'] },
+    { city: 'Osun', area: ['Aagba', 'Ada', 'Ikirun', 'Ilesa ', 'Ibokun'] },
+    { city: 'Ekiti', area: ['Ado-Ekiti', 'Oye-Ekiti', 'Ido', 'Aramoko ', 'Erio-Ekiti'] }
+  ];
+  areas = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

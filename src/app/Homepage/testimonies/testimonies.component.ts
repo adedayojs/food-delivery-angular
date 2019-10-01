a39d563b95e9f5bbc61ef9a331dca0610e2345d8
adedayojs <samfeolu@gmail.com>
2019-10-01 23:03:34 +0100

    Chore: Created testimonies component

---
+++ src/app/Homepage/testimonies/testimonies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonies',
+  templateUrl: './testimonies.component.html',
+  styleUrls: ['./testimonies.component.css']
+})
+export class TestimoniesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/cards/testimonies-card/testimonies-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonies-card',
+  templateUrl: './testimonies-card.component.html',
+  styleUrls: ['./testimonies-card.component.css']
+})
+export class TestimoniesCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

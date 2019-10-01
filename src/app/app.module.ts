4be152814d6ea653eb481435ea38f6f016e89907
adedayojs <samfeolu@gmail.com>
2019-10-01 15:53:48 +0100

    Fix: Imported Newly Created Card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { RecommendedComponent } from './recommended/recommended.component';
 import { CardComponent } from './recommended/card/card.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { HowItWorksCardComponent } from './how-it-works/card/card.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +18,11 @@ import { HowItWorksComponent } from './how-it-works/how-it-works.component';
     JumbotronComponent,
     RecommendedComponent,
     CardComponent,
-    HowItWorksComponent
+    HowItWorksComponent,
+    HowItWorksCardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
